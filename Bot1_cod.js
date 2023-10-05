ff2aad6aca50ff2d8f853b52654637eab4701546
Virus36010 <146997559+Virus36010@users.noreply.github.com>
2023-10-05 16:56:55 +0300

    Update Bot1_cod.js

--- Bot1_cod.js
+++ Bot1_cod.js
@@ -1,6 +1,6 @@
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.6387486616:AAEQSVVyEguFB6acc3zmXzERlJp98GIzKCE)
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply(''))
